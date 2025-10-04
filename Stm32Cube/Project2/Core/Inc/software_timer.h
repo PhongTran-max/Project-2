fb165adbbed3b000c6703d4da93e01dcbd0c9906
PhongTran-max <tranthanhphong.tin6@gmail.com>
2025-10-04 12:01:56 +0700

    complete

---
+++ Stm32Cube/Project2/Core/Inc/software_timer.h
@@ -0,0 +1,15 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 4, 2025
+ *      Author: NCPC
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+void setTimer(int index, int duration);
+int isTimerExpired(int index);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
